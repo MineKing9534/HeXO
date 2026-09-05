4efc72433e2e483bb7f13e6eeed40ca925d306e0
MineKing <mineking9534@gmail.com>
2026-09-05 10:22:33 +0200

    fix: BKE implicit origin

--- board/parse/src/commonTest/kotlin/de/mineking/hexo/board/parse/test/RectilinearStateBKETurnNotationParserTest.kt
+++ board/parse/src/commonTest/kotlin/de/mineking/hexo/board/parse/test/RectilinearStateBKETurnNotationParserTest.kt
@@ -6,11 +6,31 @@ import de.mineking.hexo.board.CellHighlight
 import de.mineking.hexo.board.CellOwner
 import de.mineking.hexo.board.Direction
 import de.mineking.hexo.board.LineHighlight
+import de.mineking.hexo.board.TurnMetaData
+import de.mineking.hexo.board.findNextTurn
 import de.mineking.hexo.board.parse.parseRectilinearStateBKETurnNotation
+import de.mineking.hexo.board.toGamePosition
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class RectilinearStateBKETurnNotationParserTest {
+    @Test
+    fun `follow turns after an explicit X turn following the implicit opening`() {
+        val board = "x A0 H2.2 o A1 G2.2".parseRectilinearStateBKETurnNotation(focusWinningRows = false)
+
+        val position = board.toGamePosition().turns
+        assertEquals(listOf(CellOwner.O, CellOwner.X, CellOwner.O), position.turns.map { it.meta.player })
+        assertEquals(listOf(1, 2, 2), position.turns.map { it.moves.size })
+        assertEquals(TurnMetaData(CellOwner.X, 2, 3), board.findNextTurn())
+    }
+
+    @Test
+    fun `continue an incomplete explicit X turn following the implicit opening`() {
+        val board = "x A0".parseRectilinearStateBKETurnNotation(focusWinningRows = false)
+
+        assertEquals(TurnMetaData(CellOwner.X, 1, 1), board.findNextTurn())
+    }
+
     @Test
     fun `test 1`() {
         val board = ".x/xx, b@(1,0): o A0 A1 x B3.1 B3.2".parseRectilinearStateBKETurnNotation()
